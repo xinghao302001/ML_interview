19092daad96f0ac297d2d6a7f95af29b65ab25a3
xinghao302001 <wxhlovelife@gmail.com>
2023-05-07 23:33:56 +0200

    update metrics for LR

--- metrics.py
+++ metrics.py
@@ -4,3 +4,20 @@ def acc_score(y, y_pred):
     assert y.shape[0] == y_pred.shape[0],"the size of y_true must be equal to the size of y_predict"
 
     return sum(y_pred==y) / len(y)
+
+
+def mean_squard_error(y_true, y_pred):
+    assert len(y_true) == len(y_pred), "the size of y_true must be equal to the size of y_predict"
+
+    return np.sum(y_true - y_pred ** 2) / len(y_true)
+
+
+def root_mean_squared_error(y_true, y_pred):
+    assert len(y_true) == len(y_pred), "the size of y_true must be equal to the size of y_predict"
+    return np.sqrt(mean_squard_error(y_true, y_pred))
+
+
+def mean_absolute_error(y_true, y_pred):
+    assert len(y_true) == len(y_pred), "the size of y_true must be equal to the size of y_predict"
+    return np.sum(np.absolute(y_true - y_pred)) / len(y_true)
+    
